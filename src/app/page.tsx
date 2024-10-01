27ab89aff5ac26064b1f79a68895f99bc3a28e81
TanniJaat <tprimegamers@gmail.com>
2024-10-01 22:37:45 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,99 @@
+"use client"
 import Image from "next/image";
-
+import { Source_Code_Pro } from "next/font/google";
+import { useEffect, useState } from "react";
+import Img from "./Image";
+import { ArrowLeftIcon, ArrowRight } from "lucide-react";
+const pro = Source_Code_Pro({ subsets: ["latin"], weight: "700" });
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [currentImage, setCurrentImage] = useState(0); 
+  const [images,setImages] = useState([""]);
+  const [loading, setLoading] = useState<boolean>(true);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+   
+    useEffect(() => {
+      setImages(Img);
+      const timer = setTimeout(() => {
+        setLoading(false); 
+      }, 100);
+  
+      return () => clearTimeout(timer); 
+    }, []);
+  if(loading){
+    return(
+      <div className="fixed inset-0 flex items-center justify-center  text-white z-50">
+      <h1 className="text-4xl">Loading......</h1>
+    </div>
+    )
+  }
+    return (
+    <div className="flex flex-col w-[100vw] h-[100vh] p-5 items-center">
+      <div className="flex flex-col items-center">
+        <div className={pro.className}>
+          <h1 className="pt-2 text-4xl">Image Gallery</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {/* Gallery */}
+        <div className=" flex justify-between grid-cols-3 absolute top-[50%] bottom-[50%]  pt-4 items-center gap-3 ">
+
+          {/* leftside */}
+            {
+              currentImage>=1&&(
+                <div>
+                   <Image className=" scale-75 transition-transform duration-500 opacity-40" src={images[currentImage-1]} alt={""} height={300} width={300} />
+                </div>
+              )||(
+                <div>
+                    <Image className=" scale-75 transition-transform duration-500 opacity-40" src={images[images.length-1]} alt={""} height={300} width={300} />
+                </div>
+              )
+            }
+          {/* currentImage */}
+            {
+              <div className="h-[550px] items-center justify-center flex overflow-hidden">
+                <Image className="md:scale-125 scale-150  z-[1] transition-transform duration-500" src={images[currentImage]} alt={""} height={300} width={300} />
+              </div>
+            }
+          {/* right side */}
+              {
+                  currentImage<=images.length-2&&(
+                    <div> 
+                       <Image className="scale-75 transition-transform duration-500 opacity-40" src={images[currentImage+1]} alt={""} height={300} width={300}  />
+                    </div>
+                  )||(
+                    <div>
+                       <Image className="scale-75 transition-transform duration-500 opacity-40" src={images[0]} alt={""} height={300} width={300}  />
+                    </div>
+                  )
+              }
+
+        </div>
+
+        <div className="absolute z-[10] bottom-10 gap-10 flex">
+          <button className="w-[30px] h-[30px] flex items-center justify-center bg-gray-600 rounded-full" onClick={()=>{
+            if(currentImage>0){
+              setCurrentImage(currentImage-1)
+            }
+            else{
+              setCurrentImage(images.length-1)
+            }
+          }}>
+            <ArrowLeftIcon/>
+          </button>
+
+          <div>
+            {currentImage+1}
+          </div>
+          <button className="w-[30px] h-[30px] flex items-center justify-center bg-gray-600 rounded-full" onClick={()=>{
+            if(currentImage<=images.length-2){
+              setCurrentImage(currentImage+1)
+            }else{
+              setCurrentImage(0)
+            }
+          }}>
+            <ArrowRight/>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
